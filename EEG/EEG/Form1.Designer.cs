d4f7f36727962dc0774699021c3054ce686da99a
Riccardo Fantin <rfantin@sisma.com>
2022-01-25 23:45:31 +0100

    Start coding the editor

--- EEG/EEG/Form1.Designer.cs
+++ EEG/EEG/Form1.Designer.cs
@@ -30,37 +30,165 @@ namespace EEG
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.BtnNew = new System.Windows.Forms.ToolStripButton();
+            this.BtnSave = new System.Windows.Forms.ToolStripButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.pictureBox1.Location = new System.Drawing.Point(46, 47);
+            this.pictureBox1.Location = new System.Drawing.Point(196, 28);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(538, 325);
+            this.pictureBox1.Size = new System.Drawing.Size(776, 410);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
             // 
             // timer1
             // 
             this.timer1.Enabled = true;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.BtnNew,
+            this.BtnSave});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(984, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // BtnNew
+            // 
+            this.BtnNew.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.BtnNew.Image = ((System.Drawing.Image)(resources.GetObject("BtnNew.Image")));
+            this.BtnNew.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.BtnNew.Name = "BtnNew";
+            this.BtnNew.Size = new System.Drawing.Size(37, 22);
+            this.BtnNew.Text = "NEW";
+            this.BtnNew.Click += new System.EventHandler(this.BtnNew_Click);
+            // 
+            // BtnSave
+            // 
+            this.BtnSave.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.BtnSave.Image = ((System.Drawing.Image)(resources.GetObject("BtnSave.Image")));
+            this.BtnSave.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.BtnSave.Name = "BtnSave";
+            this.BtnSave.Size = new System.Drawing.Size(37, 22);
+            this.BtnSave.Text = "SAVE";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 28);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(53, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Anger";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 66);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(66, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.Text = "Sadness";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 104);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(41, 17);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.Text = "Joy";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(12, 142);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(63, 17);
+            this.radioButton4.TabIndex = 5;
+            this.radioButton4.Text = "Surprise";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(12, 180);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(60, 17);
+            this.radioButton5.TabIndex = 6;
+            this.radioButton5.Text = "Disgust";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(12, 218);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(70, 17);
+            this.radioButton6.TabIndex = 7;
+            this.radioButton6.Text = "Contempt";
+            this.radioButton6.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(12, 256);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(46, 17);
+            this.radioButton7.TabIndex = 8;
+            this.radioButton7.Text = "Fear";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(984, 450);
+            this.Controls.Add(this.radioButton7);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.pictureBox1);
             this.DoubleBuffered = true;
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -68,6 +196,16 @@ namespace EEG
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton BtnNew;
+        private System.Windows.Forms.ToolStripButton BtnSave;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
     }
 }
 
--- EEG/EEG/Form1.cs
+++ EEG/EEG/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,16 +18,117 @@ namespace EEG
         public Form1()
         {
             InitializeComponent();
+            Polygons = new List<List<Point>>();
+
+            List<Point> RightEye = new List<Point>();
+            RightEye.Add(new Point(10, 10));
+            RightEye.Add(new Point(10, 50));
+            RightEye.Add(new Point(50, 50));
+            RightEye.Add(new Point(50, 10));
+
+            Polygons.Add(RightEye);
+
+            List<Point> LeftEye = new List<Point>();
+            LeftEye.Add(new Point(90, 10));
+            LeftEye.Add(new Point(90, 50));
+            LeftEye.Add(new Point(140, 50));
+            LeftEye.Add(new Point(140, 10));
+
+            Polygons.Add(LeftEye);
         }
 
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
-            TheFace.Paint(pictureBox1.Size.Height,pictureBox1.Size.Width, e);
+            TheFace.Paint(pictureBox1.Size.Height, pictureBox1.Size.Width, e);
+            Pen pen = new Pen(new SolidBrush(System.Drawing.Color.Yellow), 2);
+            e.Graphics.DrawLine(pen, pictureBox1.Width / 2, 0, pictureBox1.Width / 2, pictureBox1.Height);
+
+            //draw polygon
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            e.Graphics.Clear(pictureBox1.BackColor);
+
+            // Draw the old polygons.
+            foreach (List<Point> polygon in Polygons)
+            {
+                e.Graphics.FillPolygon(Brushes.White, polygon.ToArray());
+                e.Graphics.DrawPolygon(Pens.Blue, polygon.ToArray());
+
+            }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             pictureBox1.Refresh();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnNew_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Thanks to http://csharphelper.com/blog/2014/10/let-the-user-draw-polygons-in-c/ for this part of code
+
+        // Each polygon is represented by a List.
+        private List<List<Point>> Polygons = new List<List<Point>>();
+        // See if the mouse is over a polygon's edge.
+        private bool MouseIsOverEdge(Point mouse_pt,
+            out List<Point> hit_polygon, out int hit_pt1,
+            out int hit_pt2, out Point closest_point)
+        {
+            // Examine each polygon.
+            // Examine them in reverse order to check those on top first.
+            for (int pgon = Polygons.Count - 1; pgon >= 0; pgon--)
+            {
+                var polygon = Polygons[pgon];
+
+                // See if we're over one of the polygon's segments.
+                for (int p1 = 0; p1 < polygon.Count; p1++)
+                {
+                    // Get the index of the polygon's next point.
+                    int p2 = (p1 + 1) % polygon.Count;
+
+                    // See if we're over the segment between these points.
+                    PointF closest;
+                    if (FindDistanceToSegmentSquared(mouse_pt, polygon[p1], polygon[p2], out closest) < 10)
+                    {
+                        // We're over this segment.
+                        hit_polygon = polygon;
+                        hit_pt1 = p1;
+                        hit_pt2 = p2;
+                        closest_point = Point.Round(closest);
+                        return true;
+                    }
+                }
+            }
+
+            hit_polygon = null;
+            hit_pt1 = -1;
+            hit_pt2 = -1;
+            closest_point = new Point(0, 0);
+            return false;
+        }
+
+        private int FindDistanceToSegmentSquared(Point mouse_pt, Point point1, Point point2, out PointF closest)
+        {
+            double dist1 = Math.Pow(mouse_pt.X - point1.X, 2) + Math.Pow(mouse_pt.Y - point1.Y, 2);
+            double dist2 = Math.Pow(mouse_pt.X - point2.X, 2) + Math.Pow(mouse_pt.Y - point2.Y, 2);
+
+            closest = (dist1 < dist2) ? point1 : point2;
+            return (int)((dist1 < dist2) ? dist1 : dist2);
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            if (MouseIsOverEdge(new Point(e.X,e.Y), out List<Point> hit_polygon, out int hit_point, out int hit_point2, out Point closest_point))
+            {
+                
+            }
         }
     }
 }
